349b2571b6f1040a1926a88d114382cc687cbe06
Equinox-13 <quaid.johar33@gmail.com>
2019-12-28 13:03:26 +0530

    Added create, edit, delete functionality

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
-	title = models.CharField(max_length=200)
-	author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
-	body = models.TextField()
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
+    body = models.TextField()
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
+        
+# Reverse is a very handy utility function Django provides us to reference an 
+# object by its URL template name    
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
